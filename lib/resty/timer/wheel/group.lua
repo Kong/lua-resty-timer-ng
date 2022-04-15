Kong/lua-resty-timer-ng

commit afda55ed0385eeaeb79f86000a610c6272237f19

style(*) decoupling module `wheel`, `job`, and `wheel.group`

lib/resty/timer/wheel/group.lua
@@ -1,8 +1,9 @@
 local utils = require("resty.timer.utils")
 local wheel = require("resty.timer.wheel")
-local constants = require("resty.timer.constants")
 
-local math_floor = math.floor
+local table_insert = table.insert
+
+local string_format = string.format
 
 local ngx = ngx
 
@@ -14,6 +15,7 @@ local ngx_ERR = ngx.ERR
 local ngx_now = ngx.now
 local ngx_update_time = ngx.update_time
 
+local ipairs = ipairs
 local setmetatable = setmetatable
 
 -- luacheck: push ignore
@@ -32,15 +34,16 @@ function _M:update_closest()
     local old_closest = self.closest
     local delay = 0
     local lowest_wheel = self.lowest_wheel
+    local resolution = self.resolution
     local cur_msec_pointer = lowest_wheel:get_cur_pointer()
 
-    -- `constants.MSEC_WHEEL_SLOTS - 1` means
+    -- `lowest_wheel.nelts - 1` means
     -- ignore the current slot
-    for i = 1, constants.MSEC_WHEEL_SLOTS - 1 do
+    for i = 1, lowest_wheel.nelts do
         local pointer, cycles =
             lowest_wheel:cal_pointer(cur_msec_pointer, i)
 
-        delay = delay + constants.RESOLUTION
+        delay = delay + resolution
 
         -- Scan only to the end point, not the whole wheel.
         -- why?
@@ -74,20 +77,15 @@ end
 -- * add all expired jobs from wheels to `wheels.ready_jobs`
 -- * move some jobs from higher wheel to lower wheel
 function _M:fetch_all_expired_jobs()
-    local hour_wheel = self.hour_wheel
-    local minute_wheel = self.minute_wheel
-    local second_wheel = self.second_wheel
-    local msec_wheel = self.msec_wheel
-
-    utils.table_merge(self.ready_jobs, hour_wheel:fetch_all_expired_jobs())
-    utils.table_merge(self.ready_jobs, minute_wheel:fetch_all_expired_jobs())
-    utils.table_merge(self.ready_jobs, second_wheel:fetch_all_expired_jobs())
-    utils.table_merge(self.ready_jobs, msec_wheel:fetch_all_expired_jobs())
+    for _, _wheel in ipairs(self.wheels) do
+        utils.table_merge(self.ready_jobs, _wheel:fetch_all_expired_jobs())
+    end
 end
 
 
 function _M:sync_time()
     local lowest_wheel = self.lowest_wheel
+    local resolution = self.resolution
 
     -- perhaps some jobs have expired but not been fetched
     self:fetch_all_expired_jobs()
@@ -96,13 +94,13 @@ function _M:sync_time()
     self.real_time = ngx_now()
 
     local delta = self.real_time - self.expected_time
-    delta = math_floor(delta * 10)
+    delta = utils.convert_second_to_step(delta, resolution)
 
     lowest_wheel:spin_pointer(delta)
 
     self:fetch_all_expired_jobs()
 
-    self.expected_time = self.expected_time + constants.RESOLUTION * delta
+    self.expected_time = self.expected_time + resolution * delta
 end
 
 
@@ -112,8 +110,11 @@ function _M:insert_job(job)
 end
 
 
-function _M.new()
+function _M.new(wheel_setting, resolution)
     local self = {
+        setting = wheel_setting,
+        resolution = resolution,
+
         real_time = 0,
         expected_time = 0,
 
@@ -136,31 +137,52 @@ function _M.new()
         -- TODO: use `utils.table_new`
         pending_jobs = {},
 
-        hour_wheel = wheel.new(constants.HOUR_WHEEL_ID,
-                               constants.HOUR_WHEEL_SLOTS),
+        wheels = utils.table_new(wheel_setting.level, 0),
 
-        minute_wheel = wheel.new(constants.MINUTE_WHEEL_ID,
-                                 constants.MINUTE_WHEEL_SLOTS),
+        -- hour_wheel = wheel.new(constants.HOUR_WHEEL_ID,
+        --                        constants.HOUR_WHEEL_SLOTS),
 
-        second_wheel = wheel.new(constants.SECOND_WHEEL_ID,
-                                 constants.SECOND_WHEEL_SLOTS),
+        -- minute_wheel = wheel.new(constants.MINUTE_WHEEL_ID,
+        --                          constants.MINUTE_WHEEL_SLOTS),
 
-        msec_wheel = wheel.new(constants.MSEC_WHEEL_ID,
-                               constants.MSEC_WHEEL_SLOTS),
+        -- second_wheel = wheel.new(constants.SECOND_WHEEL_ID,
+        --                          constants.SECOND_WHEEL_SLOTS),
+
+        -- msec_wheel = wheel.new(constants.MSEC_WHEEL_ID,
+        --                        constants.MSEC_WHEEL_SLOTS),
     }
 
-    self.hour_wheel:set_lower_wheel(self.minute_wheel)
+    local prev_wheel = nil
+    local cur_wheel
 
-    self.minute_wheel:set_higher_wheel(self.hour_wheel)
-    self.minute_wheel:set_lower_wheel(self.second_wheel)
+    for index, slots in ipairs(wheel_setting.slots) do
+        local wheel_id = string_format("wheel#%d", index)
+        cur_wheel = wheel.new(wheel_id, slots)
 
-    self.second_wheel:set_higher_wheel(self.minute_wheel)
-    self.second_wheel:set_lower_wheel(self.msec_wheel)
+        if prev_wheel then
+            cur_wheel:set_lower_wheel(prev_wheel)
+            prev_wheel:set_higher_wheel(cur_wheel)
+        end
 
-    self.msec_wheel:set_higher_wheel(self.second_wheel)
+        table_insert(self.wheels, cur_wheel)
+        prev_wheel = cur_wheel
+    end
 
-    self.highest_wheel = self.hour_wheel
-    self.lowest_wheel = self.msec_wheel
+    self.highest_wheel = self.wheels[#self.wheels]
+    self.lowest_wheel = self.wheels[1]
+
+    -- self.hour_wheel:set_lower_wheel(self.minute_wheel)
+
+    -- self.minute_wheel:set_higher_wheel(self.hour_wheel)
+    -- self.minute_wheel:set_lower_wheel(self.second_wheel)
+
+    -- self.second_wheel:set_higher_wheel(self.minute_wheel)
+    -- self.second_wheel:set_lower_wheel(self.msec_wheel)
+
+    -- self.msec_wheel:set_higher_wheel(self.second_wheel)
+
+    -- self.highest_wheel = self.hour_wheel
+    -- self.lowest_wheel = self.msec_wheel
 
     return setmetatable(self, meta_table)
 end

lib/resty/timer/constants.lua
@@ -1,5 +1,9 @@
 local utils = require("resty.timer.utils")
 
+local math_modf = math.modf
+
+local string_format = string.format
+
 local assert = utils.assert
 
 local _M = {
@@ -10,54 +14,66 @@ local _M = {
 
     DEFAULT_FORCE_UPDATE_TIME = true,
 
-    -- 23 hour 59 minute 00 second
-    MAX_EXPIRE = 23 * 60 * 60 + 59 * 60,
-
     -- 100ms
-    RESOLUTION = 0.1,
+    DEFAULT_RESOLUTION = 0.1,
 
-    -- 100ms per slot
-    MSEC_WHEEL_SLOTS = 10,
-
-    -- 1 second per slot
-    SECOND_WHEEL_SLOTS = 60,
-
-    -- 1 minute per slot
-    MINUTE_WHEEL_SLOTS = 60,
-
-    -- 1 hour per slot
-    HOUR_WHEEL_SLOTS = 24,
-
-    MSEC_WHEEL_ID = "msec_wheel",
-
-    SECOND_WHEEL_ID = "second_wheel",
-
-    MINUTE_WHEEL_ID = "minute_wheel",
-
-    HOUR_WHEEL_ID = "hour_wheel",
+    DEFAULT_WHEEL_SETTING = {
+        level = 4,
+        slots = {10, 60, 60, 24},
+    },
 
     MSG_FATAL_FAILED_CREATE_NATIVE_TIMER
         = "failed to create a native timer: ",
 }
 
-
--- 100ms * 10 = 1 second
-assert(_M.RESOLUTION * _M.MSEC_WHEEL_SLOTS == 1,
-    "perhaps you need to update the constants " ..
-    "`RESOLUTION` and `MSEC_WHEEL_SLOTS`")
+-- We don't need a high accuracy.
+assert(_M.DEFAULT_RESOLUTION,
+    "`DEFAULT_RESOLUTION` must be greater than or equal to 0.1")
 
 
--- `-10` means don't touch the boundary, i.e. 23 hour 59 minute 00 second
--- you can also change it to `-2` (min)
-local _max_expire = _M.RESOLUTION
-    * _M.MSEC_WHEEL_SLOTS
-    * _M.SECOND_WHEEL_SLOTS
-    * _M.MINUTE_WHEEL_SLOTS
-    * _M.HOUR_WHEEL_SLOTS
-    - 10
+do
+    local wheel_setting = _M.DEFAULT_WHEEL_SETTING
+
+    assert(type(wheel_setting) == "table",
+        "`DEFAULT_WHEEL_SETTING` must be a table")
+
+    local level = wheel_setting.level
+    local slots = wheel_setting.slots
+
+    assert(type(level) == "number",
+        "`DEFAULT_WHEEL_SETTING.level` muse be a number")
+
+    assert(level >= 1,
+        "`DEFAULT_WHEEL_SETTING.level` muse be greater than or equal to 1")
+
+    local _, tmp = math_modf(level)
+
+    assert(tmp == 0,
+        "`DEFAULT_WHEEL_SETTING.level` muse be an integer")
+
+    assert(type(slots) == "table",
+        "`DEFAULT_WHEEL_SETTING.slots` muse be a table")
+
+    local slots_length = #slots
+
+    assert(level == slots_length,
+        "`DEFAULT_WHEEL_SETTING.level`"
+     .. " must be equal to "
+     .. "the length of `DEFAULT_WHEEL_SETTING.slots`")
 
 
-assert(_M.MAX_EXPIRE < _max_expire,
-    "`MAX_EXPIRE` should not exceed the maximum supported range of the wheels")
+    for i, v in ipairs(slots) do
+        assert(type(v) == "number",string_format(
+            "`DEFAULT_WHEEL_SETTING.slots[%d]` must be a number", i))
+
+        assert(v >= 1, string_format(
+            "`DEFAULT_WHEEL_SETTING.slots[%d]` must be greater than 1", i))
+
+        _, tmp = math_modf(v)
+
+        assert(tmp == 0, string_format(
+            "`DEFAULT_WHEEL_SETTING.slots[%d]` must be an integer", i))
+    end
+end
 
 return _M

lib/resty/timer/init.lua
@@ -10,6 +10,7 @@ local constants = require("resty.timer.constants")
 local ngx = ngx
 
 local math_max = math.max
+local math_min = math.min
 local math_random = math.random
 local math_modf = math.modf
 local math_huge = math.huge
@@ -32,6 +33,7 @@ local ngx_now = ngx.now
 local ngx_update_time = ngx.update_time
 
 local pairs = pairs
+local ipairs = ipairs
 local tostring = tostring
 local type = type
 local next = next
@@ -263,6 +265,7 @@ local function super_timer_callback(premature, self)
     local semaphore_super = self.semaphore_super
     local threads = self.threads
     local opt_threads = self.opt.threads
+    local opt_resolution = self.opt.resolution
     local wheels = self.wheels
 
     self.super_timer = true
@@ -277,11 +280,11 @@ local function super_timer_callback(premature, self)
         end
     end
 
-    ngx_sleep(constants.RESOLUTION)
+    ngx_sleep(opt_resolution)
 
     ngx_update_time()
     wheels.real_time = ngx_now()
-    wheels.expected_time = wheels.real_time - constants.RESOLUTION
+    wheels.expected_time = wheels.real_time - opt_resolution
 
     while not ngx_worker_exiting() and not self.destory do
         if self.enable then
@@ -292,7 +295,8 @@ local function super_timer_callback(premature, self)
             end
 
             wheels:update_closest()
-            local closest = math_max(wheels.closest, constants.RESOLUTION)
+            local closest = math_max(wheels.closest, opt_resolution)
+            closest = math_min(closest, 1)
             wheels.closest = math_huge
 
             log_notice(string_format(
@@ -306,7 +310,7 @@ local function super_timer_callback(premature, self)
             end
 
         else
-            ngx_sleep(constants.RESOLUTION)
+            ngx_sleep(0.1)
         end
     end
 end
@@ -370,7 +374,7 @@ function _M.configure(timer_sys, options)
             local _, tmp = math_modf(options.restart_thread_after_runs)
 
             assert(tmp == 0,
-                "expected `restart_thread_after_runs` to be a integer")
+                "expected `restart_thread_after_runs` to be an integer")
         end
 
         if options.threads then
@@ -381,11 +385,63 @@ function _M.configure(timer_sys, options)
             "expected `threads` to be greater than 0")
 
             local _, tmp = math_modf(options.threads)
-            assert(tmp == 0, "expected `threads` to be a integer")
+            assert(tmp == 0, "expected `threads` to be an integer")
+        end
+
+        if options.resolution then
+            assert(type(options.resolution) == "number",
+                "expected `resolution` to be a number")
+
+            assert(utils.float_compare(options.resolution, 0.1) >= 0,
+            "expected `resolution` to be greater than or equal to 0.1")
+        end
+
+        if options.wheel_setting then
+            local wheel_setting = options.wheel_setting
+            local level = wheel_setting.level
+
+            assert(type(wheel_setting) == "table",
+                "expected `wheel_setting` to be a number")
+
+            assert(type(wheel_setting.level) == "number",
+                "expected `wheel_setting.level` to be a number")
+
+            assert(type(wheel_setting.slots) == "table",
+                "expected `wheel_setting.slots` to be a table")
+
+            local slots_length = #wheel_setting.slots
+
+            assert(level == slots_length,
+                "expected `wheel_setting.level`"
+             .. " is equal to "
+             .. "the length of `wheel_setting.slots`")
+
+            for i, v in ipairs(wheel_setting.slots) do
+                assert(type(v) == "number",string_format(
+                    "expected `wheel_setting.slots[%d]` to be a number", i))
+
+                assert(v >= 1, string_format(
+                    "expected `wheel_setting.slots[%d]`"
+                 .. "to be greater than 1", i))
+
+                local _, tmp = math_modf(v)
+
+                assert(tmp == 0, string_format(
+                    "expected `wheel_setting.slots[%d]` to be an integer", i))
+            end
+
         end
     end
 
     local opt = {
+        wheel_setting = options
+            and options.wheel_setting
+            or constants.DEFAULT_WHEEL_SETTING,
+
+        resolution = options
+            and options.resolution
+            or  constants.DEFAULT_RESOLUTION,
+
         -- restart the thread after every n jobs have been run
         restart_thread_after_runs = options
             and options.restart_thread_after_runsor
@@ -404,10 +460,17 @@ function _M.configure(timer_sys, options)
 
     timer_sys.opt = opt
 
+    timer_sys.max_expire = opt.resolution
+    for _, v in ipairs(opt.wheel_setting.slots) do
+        timer_sys.max_expire = timer_sys.max_expire * v
+    end
+    timer_sys.max_expire = timer_sys.max_expire - 2
+
     -- enable/diable entire timing system
     timer_sys.enable = false
 
     timer_sys.threads = utils.table_new(opt.threads, 0)
+
     timer_sys.jobs = {}
 
     -- has the super timer already been created?
@@ -424,7 +487,7 @@ function _M.configure(timer_sys, options)
 
     timer_sys.semaphore_mover = semaphore.new()
 
-    timer_sys.wheels = wheel_group.new()
+    timer_sys.wheels = wheel_group.new(opt.wheel_setting, opt.resolution)
 
     for i = 1, timer_sys.opt.threads do
         timer_sys.threads[i] = {
@@ -490,8 +553,8 @@ function _M:once(name, callback, delay, ...)
     assert(type(delay) == "number", "expected `delay to be a number")
     assert(delay >= 0, "expected `delay` to be greater than or equal to 0")
 
-    if delay >= constants.MAX_EXPIRE
-        or (delay ~= 0 and delay < constants.RESOLUTION)then
+    if delay >= self.max_expire
+        or (delay ~= 0 and delay < self.opt.resolution)then
 
         log_notice("fallback to ngx.timer.every [delay = " .. delay .. "]")
         local ok, err = ngx_timer_at(delay, callback, ...)
@@ -514,8 +577,8 @@ function _M:every(name, callback, interval, ...)
     assert(type(interval) == "number", "expected `interval to be a number")
     assert(interval > 0, "expected `interval` to be greater than or equal to 0")
 
-    if interval >= constants.MAX_EXPIRE
-        or interval < constants.RESOLUTION then
+    if interval >= self.max_expire
+        or interval < self.opt.resolution then
 
         log_notice("fallback to ngx.timer.every [interval = "
             .. interval .. "]")

lib/resty/timer/job.lua
@@ -1,15 +1,13 @@
 local utils = require("resty.timer.utils")
-local constants = require("resty.timer.constants")
 
 local table_unpack = table.unpack
 local table_concat = table.concat
+local table_insert = table.insert
 
 local debug_getinfo = debug.getinfo
 
 local math_max = math.max
 local math_min = math.min
-local math_floor = math.floor
-local math_modf = math.modf
 local math_huge = math.huge
 
 local pcall = pcall
@@ -24,16 +22,20 @@ local ERR = ngx.ERR
 local ngx_now = ngx.now
 
 local setmetatable = setmetatable
+local tostring = tostring
+local pairs = pairs
 
+local string_format = string.format
+
+-- luacheck: push ignore
 local assert = utils.assert
+-- luacheck: pop
 
 local _M = {}
 
 
 local function job_tostring(job)
     local stats = job.stats
-    local offset = job.offset
-    local next_pointer = job.next_pointer
     local elapsed_time = stats.elapsed_time
     local meta = job.meta
 
@@ -42,24 +44,23 @@ local function job_tostring(job)
         ", enable = ",                  tostring(job._enable),
         ", cancel = ",                  tostring(job._cancel),
         ", once = ",                    tostring(job._once),
-        ", offset.hour = ",             tostring(offset.hour),
-        ", offset.minute = ",           tostring(offset.minute),
-        ", offset.second = ",           tostring(offset.second),
-        ", offset.msec = ",             tostring(offset.msec),
-        ", next.hour = ",               tostring(next_pointer
-                                                [constants.HOUR_WHEEL_ID]),
-        ", next.minute = ",             tostring(next_pointer
-                                                [constants.MINUTE_WHEEL_ID]),
-        ", next.second = ",             tostring(next_pointer
-                                                [constants.SECOND_WHEEL_ID]),
-        ", next.msec = ",               tostring(next_pointer
-                                                [constants.MSEC_WHEEL_ID]),
-        ", elapsed_time.max = ",        tostring(elapsed_time.max),
-        ", elapsed_time.min = ",        tostring(elapsed_time.min),
-        ", elapsed_time.avg = ",        tostring(elapsed_time.avg),
-        ", elapsed_time.variance = ",   tostring(elapsed_time.variance),
+        ", steps = ",                   tostring(job.steps),
         ", meta.name = ",               tostring(meta.name),
     }
+
+    for wheel_id, pointer in pairs(job.next_pointers) do
+        local str = string_format(", next_pointer.%s = %s",
+                                  tostring(wheel_id),
+                                  tostring(pointer))
+        table_insert(tbl, str)
+    end
+
+    for k, v in pairs(elapsed_time) do
+        local str = string_format(", elapsed_time.%s = %s",
+                                  tostring(k),
+                                  tostring(v))
+        table_insert(tbl, str)
+    end
 
     return table_concat(tbl)
 end
@@ -105,84 +106,9 @@ end
 
 -- Calculate the position of each pointer when the job expires
 local function job_re_cal_next_pointer(job, wheel_group)
-    local offset_hour = job.offset.hour
-    local offset_minute = job.offset.minute
-    local offset_second = job.offset.second
-    local offset_msec = job.offset.msec
-
-    local hour_wheel = wheel_group.hour_wheel
-    local minute_wheel = wheel_group.minute_wheel
-    local second_wheel = wheel_group.second_wheel
-    local msec_wheel = wheel_group.msec_wheel
     local lowest_wheel = wheel_group.lowest_wheel
 
-    local cur_hour_pointer = hour_wheel:get_cur_pointer()
-    local cur_minute_pointer = minute_wheel:get_cur_pointer()
-    local cur_second_pointer = second_wheel:get_cur_pointer()
-    local cur_msec_pointer = msec_wheel:get_cur_pointer()
-
-    local cur_pointers = {
-        cur_msec_pointer,
-        cur_second_pointer,
-        cur_minute_pointer,
-        cur_hour_pointer,
-    }
-
-    local offsets = {
-        offset_msec,
-        offset_second,
-        offset_minute,
-        offset_hour,
-    }
-
-    local next_msec_pointer,
-          next_second_pointer,
-          next_minute_pointer,
-          next_hour_pointer =
-            lowest_wheel:cal_pointer_cascade(cur_pointers, offsets)
-
-
-    -- Suppose a job will expire in one minute
-    -- and we need to spin the pointer of the `minute_wheel`,
-    -- but obviously we should not make the
-    -- second and msec pointer pointing to zero,
-    -- they should point to the current position.
-
-    if next_hour_pointer ~= 0 then
-        next_minute_pointer = next_minute_pointer == 0
-            and cur_minute_pointer or next_minute_pointer
-
-        next_second_pointer = next_second_pointer == 0
-            and cur_second_pointer or next_second_pointer
-
-        next_msec_pointer = next_msec_pointer == 0
-            and cur_msec_pointer or next_msec_pointer
-
-    elseif next_minute_pointer ~= 0 then
-        next_second_pointer = next_second_pointer == 0
-            and cur_second_pointer or next_second_pointer
-
-        next_msec_pointer = next_msec_pointer == 0
-            and cur_msec_pointer or next_msec_pointer
-
-    elseif next_second_pointer ~= 0 then
-        next_msec_pointer = next_msec_pointer == 0
-            and cur_msec_pointer or next_msec_pointer
-
-    -- else
-    --     nop
-    end
-
-
-    assert(next_hour_pointer ~= 0 or
-           next_minute_pointer ~= 0 or
-           next_second_pointer ~= 0 or
-           next_msec_pointer ~= 0, "unexpected error")
-
-    job.next_pointer[constants.HOUR_WHEEL_ID] = next_hour_pointer
-    job.next_pointer[constants.MINUTE_WHEEL_ID] = next_minute_pointer
-    job.next_pointer[constants.SECOND_WHEEL_ID] = next_second_pointer
-    job.next_pointer[constants.MSEC_WHEEL_ID] = next_msec_pointer
+    job.next_pointers = lowest_wheel:cal_pointer_cascade(job.steps)
 end
 
 
@@ -234,7 +160,7 @@ end
 
 
 function _M:get_next_pointer(wheel_id)
-    return self.next_pointer[wheel_id]
+    return self.next_pointers[wheel_id]
 end
 
 
@@ -245,31 +171,11 @@ end
 
 function _M.new(wheels, name, callback, delay, once, args)
     local delay_origin = delay
-    local offset_hour, offset_minute, offset_second, offset_msec
-    local immediate = true
+    local immediate = false
 
-    if delay ~= 0 then
-        immediate = false
-
-        delay, offset_msec = math_modf(delay)
-        offset_msec = offset_msec * 1000 + 10
-        offset_msec = math_floor(math_floor(offset_msec) / 100)
-
-        -- Arithmetically, the maximum of `offset_msec`
-        -- should be `9` now,
-        -- but due to floating point errors,
-        -- there may be some unexpected cases here.
-        -- So here we deal with it.
-        offset_msec = math_min(offset_msec, 9)
-
-        offset_hour = math_modf(delay / 60 / 60)
-        delay = delay % (60 * 60)
-
-        offset_minute = math_modf(delay / 60)
-        offset_second = delay % 60
+    if delay == 0 then
+        immediate = true
     end
-
-
 
     local self = {
         _enable = true,
@@ -279,15 +185,10 @@ function _M.new(wheels, name, callback, delay, once, args)
         name = name,
         callback = callback,
         delay = delay_origin,
-        offset = {
-            hour = offset_hour,
-            minute = offset_minute,
-            second = offset_second,
-            msec = offset_msec,
-        },
+        steps = utils.convert_second_to_step(delay, wheels.resolution),
 
         -- map from `wheel_id` to `next_pointer`
-        next_pointer = {},
+        next_pointers = {},
 
         _once = once,
         args = args,

lib/resty/timer/utils.lua
@@ -1,4 +1,5 @@
 local math_pow = math.pow
+local math_floor = math.floor
 
 local ngx = ngx
 
@@ -148,6 +149,12 @@ function _M.float_compare(left, right)
     return 0
 end
 
+
+function _M.convert_second_to_step(second, resolution)
+    return math_floor(_M.round(second / resolution, 2))
+end
+
+
 function _M.print_queue(self)
     local pending_jobs = self.wheels.pending_jobs
     local ready_jobs = self.wheels.ready_jobs
@@ -231,9 +238,9 @@ function _M.print_wheel(wheels)
 end
 
 
---[[ function _M.round(value, digits)
-    local x = 10 * digits
-    return floor(value * x + 0.5) / x
-end ]]
+function _M.round(value, digits)
+    local x = 10 ^ digits
+    return math_floor(value * x + 0.5) / x
+end
 
 return _M

lib/resty/timer/wheel/init.lua
@@ -2,9 +2,6 @@ local utils = require("resty.timer.utils")
 
 local math_floor = math.floor
 
-local table_insert = table.insert
-local table_unpack = table.unpack
-
 local ngx = ngx
 
 -- luacheck: push ignore
@@ -54,30 +51,30 @@ function _M:cal_pointer(pointer, offset)
 end
 
 
-function _M:cal_pointer_cascade(pointers, offsets)
-    local _pointers = {}
-    local wheel = self
-    local cycles = 0
-    for i = 1, #pointers do
-        assert(offsets[i] ~= nil, "unexpected error")
-        assert(wheel ~= nil, "unexpected error")
+function _M:cal_pointer_cascade(steps)
+    local next_pointers = { }
+    local cur_wheel = self
 
-        local p
+    local steps_for_cur_wheel = steps
+    local steps_for_next_wheel
 
-        if offsets[i] == 0 and cycles == 0 then
-            p = 0
-            cycles = 0
+    while cur_wheel do
+        local pointer = 0
+        steps_for_next_wheel = 0
 
-        else
-            p, cycles = wheel:cal_pointer(pointers[i], offsets[i] + cycles)
+        if steps_for_cur_wheel ~= 0 then
+            pointer, steps_for_next_wheel =
+            cur_wheel:cal_pointer(cur_wheel:get_cur_pointer(),
+                                  steps_for_cur_wheel)
         end
 
-        table_insert(_pointers, p)
+        next_pointers[cur_wheel.id] = pointer
 
-        wheel = wheel.higher_wheel
+        steps_for_cur_wheel = steps_for_next_wheel
+        cur_wheel = cur_wheel.higher_wheel
     end
 
-    return table_unpack(_pointers)
+    return next_pointers
 end
 
 

commit 0023604aab3d49d77c5f69d2b0da1c3e5b9b609c

fix(*) excessive error
This bug was introduced by afda55ed0385eeaeb79f86000a610c6272237f19 .

lib/resty/timer/wheel/group.lua
@@ -1,6 +1,8 @@
 local utils = require("resty.timer.utils")
 local wheel = require("resty.timer.wheel")
 
+local math_floor = math.floor
+
 local table_insert = table.insert
 
 local string_format = string.format
@@ -94,9 +96,10 @@ function _M:sync_time()
     self.real_time = ngx_now()
 
     local delta = self.real_time - self.expected_time
-    delta = utils.convert_second_to_step(delta, resolution)
+    local steps = utils.convert_second_to_step(delta, resolution)
+    delta = math_floor(delta * 10)
 
-    lowest_wheel:spin_pointer(delta)
+    lowest_wheel:spin_pointer(steps)
 
     self:fetch_all_expired_jobs()
 