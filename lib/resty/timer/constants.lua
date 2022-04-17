Kong/lua-resty-timer-ng

commit 02130194d039b789116dcf177e1512ca040df09b

style(*) improve style

lib/resty/timer/constants.lua
@@ -29,13 +29,16 @@ local _M = {
         slots_for_each_level = {10, 60, 60, 24},
     },
 
+    MIN_RESOLUTION = 0.1,
+
     MSG_FATAL_FAILED_CREATE_NATIVE_TIMER
         = "failed to create a native timer: ",
 }
 
 -- We don't need a high accuracy.
-assert(_M.DEFAULT_RESOLUTION,
-    "`DEFAULT_RESOLUTION` must be greater than or equal to 0.1")
+assert(_M.DEFAULT_RESOLUTION < _M.MIN_RESOLUTION,
+    "`DEFAULT_RESOLUTION` must be greater than "
+ .. "or equal to `MIN_RESOLUTION`")
 
 
 do

lib/resty/timer/init.lua
@@ -307,8 +307,8 @@ local function super_timer_callback(premature, self)
             local closest = wheels.closest
             wheels.closest = math_huge
 
-            if closest < 0.1 then
-                closest = 0.1
+            if closest < constants.MIN_RESOLUTION then
+                closest = constants.MIN_RESOLUTION
             end
 
             if closest > 1 then
@@ -327,7 +327,7 @@ local function super_timer_callback(premature, self)
             end
 
         else
-            ngx_sleep(0.1)
+            ngx_sleep(constants.MIN_RESOLUTION)
         end
     end
 end

commit 4b99c89eff34c70d5cebcaa285aadf5f101fa7c6

fix(constants) fix an assertion
this bug was introduced by 02130194d039b789116dcf177e1512ca040df09b

lib/resty/timer/constants.lua
@@ -36,7 +36,7 @@ local _M = {
 }
 
 -- We don't need a high accuracy.
-assert(_M.DEFAULT_RESOLUTION < _M.MIN_RESOLUTION,
+assert(_M.DEFAULT_RESOLUTION >= _M.MIN_RESOLUTION,
     "`DEFAULT_RESOLUTION` must be greater than "
  .. "or equal to `MIN_RESOLUTION`")
 