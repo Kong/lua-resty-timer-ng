Kong/lua-resty-timer-ng

commit 8bbffe283204d7c7c6e482d931b440b06fde5ba4

fix(*) for https://github.com/Kong/lua-resty-timer-ng/pull/1#discussion_r854672003

lib/resty/timer/init.lua
@@ -34,6 +34,7 @@ local ipairs = ipairs
 local tostring = tostring
 local type = type
 local next = next
+local select = select
 
 local assert = utils.assert
 
@@ -291,7 +292,7 @@ local function super_timer_callback(premature, self)
 end
 
 
-local function create(self, name, callback, delay, once, args)
+local function create(self, name, callback, delay, once, argc, argv)
     local wheels = self.wheels
     local jobs = self.jobs
     if not name then
@@ -307,7 +308,9 @@ local function create(self, name, callback, delay, once, args)
 
     wheels:sync_time()
 
-    local job = job_module.new(wheels, name, callback, delay, once, args)
+    local job = job_module.new(wheels, name,
+                               callback, delay,
+                               once, argc, argv)
     job:enable()
     jobs[name] = job
 
@@ -547,7 +550,8 @@ function _M:once(name, callback, delay, ...)
 
     -- TODO: desc the logic and add related tests
     local name_or_false, err =
-        create(self, name, callback, delay, true, { ... })
+        create(self, name, callback, delay,
+               true, select("#", ...), { ... })
 
     return name_or_false, err
 end
@@ -570,7 +574,8 @@ function _M:every(name, callback, interval, ...)
     end
 
     local name_or_false, err =
-        create(self, name, callback, interval, false, { ... })
+        create(self, name, callback, interval,
+               false, select("#", ...), { ... })
 
     return name_or_false, err
 end

lib/resty/timer/job.lua
@@ -169,7 +169,7 @@ function _M:re_cal_next_pointer(wheels)
 end
 
 
-function _M.new(wheels, name, callback, delay, once, args)
+function _M.new(wheels, name, callback, delay, once, argc, argv)
     local delay_origin = delay
     local immediate = false
 
@@ -191,7 +191,8 @@ function _M.new(wheels, name, callback, delay, once, args)
         next_pointers = {},
 
         _once = once,
-        args = args,
+        argc = argc,
+        argv = argv,
         stats = {
             elapsed_time = {
                 avg = 0,
@@ -232,7 +233,8 @@ function _M:execute()
 
     self._running = true
 
-    local ok, err = pcall(self.callback, false, table_unpack(self.args))
+    local ok, err = pcall(self.callback, false,
+                          table_unpack(self.argv, 1, self.argc))
 
     local finish = stats.finish
 

commit 0b4e9b0a78d372a9d7809184f887b5a0201adf11

fix(*) bug of `timer:run()`
this bug was introduced by 8bbffe283204d7c7c6e482d931b440b06fde5ba4

/coverage

lib/resty/timer/init.lua
@@ -599,7 +599,8 @@ function _M:run(name)
 
     local name_or_false, err =
         create(self, old_job.name, old_job.callback,
-               old_job.delay, old_job:is_oneshot(), old_job.args)
+               old_job.delay, old_job:is_oneshot(),
+               old_job.argc, old_job.argv)
 
     local ok = name_or_false ~= false
 