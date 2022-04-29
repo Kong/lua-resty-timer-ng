2f2052c16383d33260c2752909a41fbab0587f59
Qi <add_sp@outlook.com>
2022-04-29 15:31:06 +0800

    style(*) more debug friendly

--- lib/resty/timer/thread/loop.lua
+++ lib/resty/timer/thread/loop.lua
@@ -84,6 +84,52 @@ local ACTION_EXIT = 3
 local ACTION_EXIT_WITH_MSG = 4
 local ACTION_RESTART = 5
 
+local LOG_FORMAT_MAP = {
+    init = {
+        [ACTION_ERROR]              = LOG_FORMAT_ERROR_INIT,
+        [ACTION_EXIT]               = LOG_FORMAT_EXIT_INIT,
+        [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_INIT,
+        [ACTION_RESTART]            = LOG_FORMAT_RESTART_INIT,
+    },
+
+    before = {
+        [ACTION_ERROR]              = LOG_FORMAT_ERROR_BEFORE,
+        [ACTION_EXIT]               = LOG_FORMAT_EXIT_BEFORE,
+        [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_BEFORE,
+        [ACTION_RESTART]            = LOG_FORMAT_RESTART_BEFORE,
+    },
+
+    loop_body = {
+        [ACTION_ERROR]              = LOG_FORMAT_ERROR_LOOP_BODY,
+        [ACTION_EXIT]               = LOG_FORMAT_EXIT_LOOP_BODY,
+        [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_LOOP_BODY,
+        [ACTION_RESTART]            = LOG_FORMAT_RESTART_LOOP_BODY,
+    },
+
+    after = {
+        [ACTION_ERROR]              = LOG_FORMAT_ERROR_AFTER,
+        [ACTION_EXIT]               = LOG_FORMAT_EXIT_AFTER,
+        [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_AFTER,
+        [ACTION_RESTART]            = LOG_FORMAT_RESTART_AFTER,
+    },
+
+    finally = {
+        [ACTION_ERROR]              = LOG_FORMAT_ERROR_FINALLY,
+        [ACTION_EXIT]               = LOG_FORMAT_EXIT_FINALLY,
+        [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_FINALLY,
+        [ACTION_RESTART]            = LOG_FORMAT_RESTART_FINALLY,
+    },
+}
+
+local NEED_CHECK_WORKER_EIXTING = {
+    init = false,
+    before = true,
+    loop_body = true,
+    after = false,
+    finally = false,
+}
+
+
 
 local _M = {
     ACTION_CONTINUE = ACTION_CONTINUE,
@@ -119,13 +165,69 @@ local function nop_finally()
 end
 
 
+local PAHSE_HANDLERS = {
+    init = nop_init,
+    before = nop_before,
+    loop_body = nop_loop_body,
+    after = nop_after,
+    finally = nop_finally,
+}
+
+
+local function phase_handler_wrapper(self, phase)
+    local ok, action_or_err, err_or_nil =
+        pcall(self[phase].callback,
+              self.context,
+              table_unpack(self[phase].argv, 1, self[phase].argc))
+
+    if not ok then
+        return ACTION_ERROR, action_or_err
+    end
+
+    local action = action_or_err
+    local err = err_or_nil
+
+    if action == ACTION_CONTINUE or
+       action == ACTION_RESTART
+    then
+        if  self[phase].need_check_worker_exiting and
+            ngx_worker_exiting()
+        then
+            return ACTION_EXIT_WITH_MSG, "worker exiting"
+        end
+
+        if self._kill then
+            return ACTION_EXIT_WITH_MSG, "killed"
+        end
+
+        return action
+    end
+
+    if action == ACTION_EXIT then
+        return action
+    end
+
+    if action == ACTION_EXIT_WITH_MSG then
+        assert(err ~= nil)
+        return action, err
+    end
+
+    if action == ACTION_ERROR then
+        assert(err ~= nil)
+        return ACTION_ERROR, err
+    end
+
+    error("unexpected error")
+end
+
+
 ---exec phase_handler and handle its result
 ---@param self table
----@param phase_handler function self.init/before/loop_body/after/finally
+---@param phase string init/before/loop_body/after/finally
 ---@return boolean need_to_exit_thread
-local function do_phase_handler(self, phase_handler)
-    local action, err = phase_handler()
-    local log_format = self.log_format_map[phase_handler][action]
+local function do_phase_handler(self, phase)
+    local action, err = phase_handler_wrapper(self, phase)
+    local log_format = LOG_FORMAT_MAP[phase][action]
 
     if action == ACTION_CONTINUE then
         return false
@@ -160,48 +262,6 @@ local function do_phase_handler(self, phase_handler)
 end
 
 
-local function callback_wrapper(self, check_worker_exiting, callback, ...)
-    local ok, action_or_err, err_or_nil = pcall(callback, self.context, ...)
-
-    if not ok then
-        return ACTION_ERROR, action_or_err
-    end
-
-    local action = action_or_err
-    local err = err_or_nil
-
-    if action == ACTION_CONTINUE or
-       action == ACTION_RESTART
-    then
-        if check_worker_exiting and ngx_worker_exiting() then
-            return ACTION_EXIT_WITH_MSG, "worker exiting"
-        end
-
-        if self._kill then
-            return ACTION_EXIT_WITH_MSG, "killed"
-        end
-
-        return action
-    end
-
-    if action == ACTION_EXIT then
-        return action
-    end
-
-    if action == ACTION_EXIT_WITH_MSG then
-        assert(err ~= nil)
-        return action, err
-    end
-
-    if action == ACTION_ERROR then
-        assert(err ~= nil)
-        return ACTION_ERROR, err
-    end
-
-    error("unexpected error")
-end
-
-
 local function loop_wrapper(premature, self)
     if premature then
         return
@@ -211,43 +271,28 @@ local function loop_wrapper(premature, self)
             string_format(LOG_FORMAT_START,
                           self.name))
 
-    local before = self.before
-    local loop_body = self.loop_body
-    local after = self.after
-
-    if not do_phase_handler(self, self.init) then
+    if not do_phase_handler(self, "init") then
         while not ngx_worker_exiting() and not self._kill do
-            if do_phase_handler(self, before) then
+            if do_phase_handler(self, "before") then
                 break
             end
 
-            if do_phase_handler(self, loop_body) then
+            if do_phase_handler(self, "loop_body") then
                 break
             end
 
-            if do_phase_handler(self, after) then
+            if do_phase_handler(self, "after") then
                 break
             end
         end
     end
 
 
-    do_phase_handler(self, self.finally)
+    do_phase_handler(self, "finally")
 
     ngx_log(ngx_NOTICE,
             string_format(LOG_FORMAT_EXIT,
                           self.name))
-end
-
-
-local function wrap_callback(self, callback, argc, argv,
-                             is_check_worker_exiting)
-    return function ()
-        return callback_wrapper(self,
-                                is_check_worker_exiting,
-                                callback,
-                                table_unpack(argv, 1, argc))
-    end
 end
 
 
@@ -278,94 +323,26 @@ function _M.new(name, options)
     local self = {
         name = tostring(name),
         context = {},
-        _kill = false,
-        init = nop_init,
-        before = nop_before,
-        loop_body = nop_loop_body,
-        after = nop_after,
-        finally = nop_finally,
+        _kill = false
     }
 
-    local check_worker_exiting = true
-    local do_not_check_worker_exiting = false
+    for phase, default_handler in pairs(PAHSE_HANDLERS) do
+        self[phase] = {}
 
-    if options.init then
-        self.init = wrap_callback(self,
-                                  options.init.callback,
-                                  options.init.argc,
-                                  options.init.argv,
-                                  do_not_check_worker_exiting)
+        self[phase].need_check_worker_exiting
+                = NEED_CHECK_WORKER_EIXTING[phase]
+
+        if not options[phase] then
+            self[phase].argc = 0
+            self[phase].argv = {}
+            self[phase].callback = default_handler
+
+        else
+            self[phase].argc = options[phase].argc
+            self[phase].argv = options[phase].argv
+            self[phase].callback = options[phase].callback
+        end
     end
-
-    if options.before then
-        self.before = wrap_callback(self,
-                                    options.before.callback,
-                                    options.before.argc,
-                                    options.before.argv,
-                                    check_worker_exiting)
-    end
-
-    if options.loop_body then
-        self.loop_body = wrap_callback(self,
-                                       options.loop_body.callback,
-                                       options.loop_body.argc,
-                                       options.loop_body.argv,
-                                       check_worker_exiting)
-    end
-
-    if options.after then
-        self.after = wrap_callback(self,
-                                   options.after.callback,
-                                   options.after.argc,
-                                   options.after.argv,
-                                   do_not_check_worker_exiting)
-    end
-
-    if options.finally then
-        self.finally = wrap_callback(self,
-                                    options.finally.callback,
-                                    options.finally.argc,
-                                    options.finally.argv,
-                                    do_not_check_worker_exiting)
-    end
-
-
-    self.log_format_map = {
-        [self.init] = {
-            [ACTION_ERROR]              = LOG_FORMAT_ERROR_INIT,
-            [ACTION_EXIT]               = LOG_FORMAT_EXIT_INIT,
-            [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_INIT,
-            [ACTION_RESTART]            = LOG_FORMAT_RESTART_INIT,
-        },
-
-        [self.before] = {
-            [ACTION_ERROR]              = LOG_FORMAT_ERROR_BEFORE,
-            [ACTION_EXIT]               = LOG_FORMAT_EXIT_BEFORE,
-            [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_BEFORE,
-            [ACTION_RESTART]            = LOG_FORMAT_RESTART_BEFORE,
-        },
-
-        [self.loop_body] = {
-            [ACTION_ERROR]              = LOG_FORMAT_ERROR_LOOP_BODY,
-            [ACTION_EXIT]               = LOG_FORMAT_EXIT_LOOP_BODY,
-            [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_LOOP_BODY,
-            [ACTION_RESTART]            = LOG_FORMAT_RESTART_LOOP_BODY,
-        },
-
-        [self.after] = {
-            [ACTION_ERROR]              = LOG_FORMAT_ERROR_AFTER,
-            [ACTION_EXIT]               = LOG_FORMAT_EXIT_AFTER,
-            [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_AFTER,
-            [ACTION_RESTART]            = LOG_FORMAT_RESTART_AFTER,
-        },
-
-        [self.finally] = {
-            [ACTION_ERROR]              = LOG_FORMAT_ERROR_FINALLY,
-            [ACTION_EXIT]               = LOG_FORMAT_EXIT_FINALLY,
-            [ACTION_EXIT_WITH_MSG]      = LOG_FORMAT_EXIT_WITH_MSG_FINALLY,
-            [ACTION_RESTART]            = LOG_FORMAT_RESTART_FINALLY,
-        },
-    }
 
     return setmetatable(self, meta_table)
 end
